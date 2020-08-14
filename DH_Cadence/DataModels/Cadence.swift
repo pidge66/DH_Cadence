509e5a08c2f281780907b74ad7108b8283516d2b
David Hou <dhou@pa.gov>
2020-08-14 14:52:49 -0400

    Reordered sound file index

--- DH_Cadence/DataModels/Cadence.swift
+++ DH_Cadence/DataModels/Cadence.swift
@@ -30,33 +30,53 @@ struct Cadence : Identifiable {//}, Equatable {
 class ArrayCadences: ObservableObject {
     
     @Published var cadences: [Cadence]
-    static let tones = ["250Hz.mp3", "440Hz.mp3", "1000Hz.mp3", "Beep.mp3", "Slap.mp3", "Gong1.mp3", "Gong2.mp3", "Gong3.mp3", "Chime1.mp3", "silence.mp3", "Ding.mp3"]
+    static let tones = [
+        "silence.mp3",  //0
+        "250Hz.mp3",
+        "440Hz.mp3",
+        "1000Hz.mp3",
+        "Beep.mp3",
+        "Slap.mp3",
+        "Gong1.mp3",
+        "Gong2.mp3",
+        "Gong3.mp3",
+        "Chime1.mp3",
+        "Ding.mp3",     // 10
+        "Beep1.mp3",
+        "Beep2.mp3",
+        "Beep3.mp3",
+        "Beep4.mp3",
+        "Gun1.mp3",
+        "Gun2.mp3",
+        "Gun3.mp3",
+        "AirHorn.mp3"   //18
+]
     static let tempos = [30.0, 60.0, 120.0, 240.0, 480.0]
 
     init() {
         self.cadences = [
             Cadence(name: "478 Breathing Technique", repetitions: 10, metronomes: [
-                Metronome(tone: ArrayCadences.tones[1], selectedToneIndex:1, tempo: 60.0, repetitions: 3),
-                Metronome(tone: ArrayCadences.tones[4], selectedToneIndex:4, tempo: 60.0, repetitions: 1),
-                Metronome(tone: ArrayCadences.tones[3], selectedToneIndex:3, tempo: 60.0, repetitions: 6),
-                Metronome(tone: ArrayCadences.tones[4], selectedToneIndex:4, tempo: 60.0, repetitions: 1),
-                Metronome(tone: ArrayCadences.tones[7], selectedToneIndex:7, tempo: 60.0, repetitions: 7),
-                Metronome(tone: ArrayCadences.tones[4], selectedToneIndex:4, tempo: 60.0, repetitions: 1)
+                Metronome(tone: ArrayCadences.tones[2], selectedToneIndex:2, tempo: 60.0, repetitions: 3),
+                Metronome(tone: ArrayCadences.tones[5], selectedToneIndex:5, tempo: 60.0, repetitions: 1),
+                Metronome(tone: ArrayCadences.tones[4], selectedToneIndex:4, tempo: 60.0, repetitions: 6),
+                Metronome(tone: ArrayCadences.tones[5], selectedToneIndex:5, tempo: 60.0, repetitions: 1),
+                Metronome(tone: ArrayCadences.tones[8], selectedToneIndex:8, tempo: 60.0, repetitions: 7),
+                Metronome(tone: ArrayCadences.tones[5], selectedToneIndex:5, tempo: 60.0, repetitions: 1)
             ]),
             Cadence(name: "Dumb Bell Workout", repetitions: 8, metronomes: [
-                Metronome(tone: ArrayCadences.tones[1], selectedToneIndex:1, tempo: 60.0, repetitions: 3),
-                Metronome(tone: ArrayCadences.tones[10], selectedToneIndex:10, tempo: 60.0, repetitions: 1),
-                Metronome(tone: ArrayCadences.tones[9], selectedToneIndex:9, tempo: 60.0, repetitions: 1),
-                Metronome(tone: ArrayCadences.tones[4], selectedToneIndex:4, tempo: 300.0, repetitions: 7),
-                Metronome(tone: ArrayCadences.tones[8], selectedToneIndex:8, tempo: 300.0, repetitions: 1),
-                Metronome(tone: ArrayCadences.tones[9], selectedToneIndex:9, tempo: 300.0, repetitions: 3),
+                Metronome(tone: ArrayCadences.tones[2], selectedToneIndex:2, tempo: 60.0, repetitions: 3),
+                Metronome(tone: ArrayCadences.tones[11], selectedToneIndex:11, tempo: 60.0, repetitions: 1),
+                Metronome(tone: ArrayCadences.tones[0], selectedToneIndex:0, tempo: 60.0, repetitions: 1),
+                Metronome(tone: ArrayCadences.tones[5], selectedToneIndex:5, tempo: 300.0, repetitions: 7),
+                Metronome(tone: ArrayCadences.tones[9], selectedToneIndex:9, tempo: 300.0, repetitions: 1),
+                Metronome(tone: ArrayCadences.tones[0], selectedToneIndex:0, tempo: 300.0, repetitions: 3),
 
-                Metronome(tone: ArrayCadences.tones[1], selectedToneIndex:1, tempo: 60.0, repetitions: 3),
-                Metronome(tone: ArrayCadences.tones[10], selectedToneIndex:10, tempo: 60.0, repetitions: 1),
-                Metronome(tone: ArrayCadences.tones[9], selectedToneIndex:9, tempo: 60.0, repetitions: 1),
-                Metronome(tone: ArrayCadences.tones[4], selectedToneIndex:4, tempo: 300.0, repetitions: 7),
-                Metronome(tone: ArrayCadences.tones[8], selectedToneIndex:8, tempo: 300.0, repetitions: 1),
-                Metronome(tone: ArrayCadences.tones[9], selectedToneIndex:9, tempo: 300.0, repetitions: 6)
+                Metronome(tone: ArrayCadences.tones[2], selectedToneIndex:2, tempo: 60.0, repetitions: 3),
+                Metronome(tone: ArrayCadences.tones[11], selectedToneIndex:11, tempo: 60.0, repetitions: 1),
+                Metronome(tone: ArrayCadences.tones[0], selectedToneIndex:0, tempo: 60.0, repetitions: 1),
+                Metronome(tone: ArrayCadences.tones[5], selectedToneIndex:5, tempo: 300.0, repetitions: 7),
+                Metronome(tone: ArrayCadences.tones[9], selectedToneIndex:9, tempo: 300.0, repetitions: 1),
+                Metronome(tone: ArrayCadences.tones[0], selectedToneIndex:0, tempo: 300.0, repetitions: 6),
 
             ])
         ]
